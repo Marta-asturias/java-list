9eea16adecac1cbf975e4228341d0cc8a829ee47
Marta-asturias <marta.gonzalez@domoticadavinci.com>
2022-09-20 20:31:12 +0200

    primer caso

--- src/main/java/com/javalist/App.java
+++ src/main/java/com/javalist/App.java
@@ -1,10 +1,5 @@
 package com.javalist;
 
-import java.util.ArrayList;
-
-/**
- * Hello world!
- */
 public final class App {
     private App() {
     }
@@ -14,26 +9,7 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-/*         String[] arrayString = new String[] {"lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"};
-    
-
-        for (int i = 0; i < arrayString.length; i++){
-            System.out.println(arrayString[i]);
-            
-        };
-        System.out.println(arrayString.length);
-
-
-        System.out.println(arrayString[2]); */
         Listas listas = new Listas();
-        
-        
-        
-    
-          
-
         System.out.println(listas.getDays());;
-
-         
     }
 }
